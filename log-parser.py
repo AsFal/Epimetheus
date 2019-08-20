f2420bbecdb8cf8637f0ca1046aaed271d09c8d2
Alexandre <alexandre.h.falardeau@gmail.com>
2019-08-19 22:10:57 -0400

    chore: cleanup

--- log-parser.py
+++
@@ -1,6 +0,0 @@
-class CategoryTree(Tree):
-    def __init__(self, category: str):
-        super().__init__()
-        # use some form of recursive algorithm here to get my tree
-    def merge(self, categoryTree):
-        return None
