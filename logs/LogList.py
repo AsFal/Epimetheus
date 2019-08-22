ebf6c4a1fc90b0eb2423af647cbe980b82c3b316
Alexandre <alexandre.h.falardeau@gmail.com>
2019-08-21 22:00:53 -0400

    refactor: Changed LogList to no longer inherit from list

--- logs/LogList.py
+++ logs/LogList.py
@@ -1,18 +1,35 @@
 from .TreeLog import TreeLog
 
+import collections
 from functools import reduce
 
 from util import flatten
 
-class LogList(list):
+class LogList(collections.MutableSequence):
     def __init__(self, logs=[]):
+        self.list = list()
+        # self.extend(list(args))
         for log in logs:
             self.append(log)
-        super.__init__()
+        super().__init__()
 
-    def append(self, new):
-        assert isinstance(new, TreeLog)
-        super().append(new)
+    def check(self, v):
+        if not isinstance(v, TreeLog):
+            raise TypeError(v)
+
+    def __len__(self): return len(self.list)
+
+    def __getitem__(self, i): return self.list[i]
+
+    def __delitem__(self, i): del self.list[i]
+
+    def __setitem__(self, i, v):
+        self.check(v)
+        self.list[i] = v
+
+    def insert(self, i, v):
+        self.check(v)
+        self.list.insert(i, v)
 
     def getReport(self, category):
         # TODO: getAllCategoryTrees need to be accesssible to this scope
@@ -22,14 +39,13 @@ class LogList(list):
         # I.e. I should be able to TextLog.merge(TreeLog) or vice versa -> this will require some thinking
         # That would require a review of the Merge function
         # For now this will do
-        categoryTrees = [log._getAllCategoryTrees() for log in self]
+        categoryTrees = [log._getAllCategoryTrees() for log in self.list]
         categoryTreesF = [categoryTree for categoryTree in categoryTrees if categoryTree._rootNode.data.isEquivalent(category)]
         reportTree = reduce(lambda merged, categoryTree: merged.merge(categoryTree), categoryTreesF)
         return reportTree.toString()
 
-    def getAllCategoryTitles(self):
-        return flatten([log.getAllCategories() for log in self])
+    def getAllCategories(self):
+        return flatten([log.getAllCategories() for log in self.list])
 
     def getAllCategoryNames(self):
-        return flatten([log.getAllcategoryNames() for log in self])
-
+        return flatten([log.getAllcategoryNames() for log in self.list])
